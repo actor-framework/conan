c25c8ad2f5acf0ddcaf12404055eba84f2930c14
Eric Pederson <ericacm+github@gmail.com>
2017-09-18 01:29:52 -0400

    Add libstdc++11 to compiler settings in build.py

--- build.py
+++ build.py
@@ -4,4 +4,10 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds()
+    filtered_builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        if settings['compiler'] == 'gcc':
+            settings['compiler.libcxx'] = 'libstdc++11'
+        filtered_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = filtered_builds
     builder.run()
