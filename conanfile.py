0661bfd6c5a179f41c7c120443215994b7b7e3f7
Eric Pederson <ericacm+github@gmail.com>
2017-09-17 20:02:05 -0400

    Initial binary / conan-center setup

--- conanfile.py
+++ conanfile.py
@@ -10,17 +10,25 @@ class CAFConan(ConanFile):
     version = '0.15.3'
 
     name = "caf"
-    url = "https://github.com/sourcedelica/conan-recipes/tree/master/caf"
+    description = "An open source implementation of the Actor Model in C++"
+    url = "http://actor-framework.org"
     license = "BSD-3-Clause"
+
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "log_level": ["NONE", "ERROR", "WARNING", "INFO", "DEBUG", "TRACE"]}
     default_options = "shared=False", "log_level=NONE"
     source_dir = "actor-framework"
 
-    def config_options(self):
-        if self.settings.compiler == 'gcc' and float(self.settings.compiler.version.value) >= 5.1:
+    def configure(self):
+        if self.settings.compiler == "gcc" and self.settings.compiler.version < "4.8":
+            raise ConanException("g++ >= 4.8 is required")
+        else:
             if self.settings.compiler.libcxx != 'libstdc++11':
                 raise ConanException("You must use the setting compiler.libcxx=libstdc++11")
+        if self.settings.compiler == "clang" and self.settings.compiler.version < "3.4":
+            raise ConanException("g++ >= 3.4 is required")
+        if self.settings.compiler == "Visual Studio" and self.settings.compiler.version < "14":
+            raise ConanException("Visual Studio >= 14 is required")
 
     def source(self):
         self.run_command("git clone https://github.com/actor-framework/actor-framework.git")
@@ -40,11 +48,12 @@ class CAFConan(ConanFile):
         self.run(cmd, True, cwd)
 
     def package(self):
-        self.copy("*.hpp",   dst="include/caf", src="%s/libcaf_core/caf" % self.source_dir)
-        self.copy("*.hpp",   dst="include/caf", src="%s/libcaf_io/caf" % self.source_dir)
-        self.copy("*.dylib", dst="lib",         src="%s/build/lib" % self.source_dir)
-        self.copy("*.so",    dst="lib",         src="%s/build/lib" % self.source_dir)
-        self.copy("*.a",     dst="lib",         src="%s/build/lib" % self.source_dir)
+        self.copy("*.hpp",    dst="include/caf", src="%s/libcaf_core/caf" % self.source_dir)
+        self.copy("*.hpp",    dst="include/caf", src="%s/libcaf_io/caf" % self.source_dir)
+        self.copy("*.dylib",  dst="lib",         src="%s/build/lib" % self.source_dir)
+        self.copy("*.so",     dst="lib",         src="%s/build/lib" % self.source_dir)
+        self.copy("*.a",      dst="lib",         src="%s/build/lib" % self.source_dir)
+        self.copy("license*", dst="licenses", ignore_case=True, keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["caf_io_static", "caf_core_static"]
