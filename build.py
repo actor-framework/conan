75b1f3596c17c90495cd06f44619158198a553a3
Eric Pederson <ericacm+github@gmail.com>
2017-09-19 20:03:13 -0400

    Comment out filtered_builds stuff

--- build.py
+++ build.py
@@ -4,12 +4,12 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds()
-    filtered_builds = []
-    for settings, options, env_vars, build_requires in builder.builds:
-        if settings['compiler'] == 'gcc':
-            settings['compiler.libcxx'] = 'libstdc++11'
-            # TODO - re-enable other builds
-            if settings['compiler.version'] == 5.4:
-                filtered_builds.append([settings, options, env_vars, build_requires])
-    builder.builds = filtered_builds
+    # filtered_builds = []
+    # for settings, options, env_vars, build_requires in builder.builds:
+    #     if settings['compiler'] == 'gcc':
+    #         settings['compiler.libcxx'] = 'libstdc++11'
+    #         # TODO - re-enable other builds
+    #         if settings['compiler.version'] == 5.4:
+    #             filtered_builds.append([settings, options, env_vars, build_requires])
+    # builder.builds = filtered_builds
     builder.run()
