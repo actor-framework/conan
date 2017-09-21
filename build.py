de99f4011e07801b8557f55f865ae4ddebc25df5
Eric Pederson <ericacm+github@gmail.com>
2017-09-20 23:11:22 -0400

    Add CONAN_LINUX_PLATFORM check in build.py

--- build.py
+++ build.py
@@ -1,9 +1,13 @@
-from conan.packager import ConanMultiPackager
+from conan.packager import ConanMultiPackager, PlatformInfo
 import linux_platform
+import os
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(platform_info=linux_platform)
+    # For testing Docker on Mac
+    platform_info = linux_platform if os.getenv('CONAN_LINUX_PLATFORM') else PlatformInfo()
+
+    builder = ConanMultiPackager(platform_info=platform_info)
     builder.add_common_builds()
     filtered_builds = []
 
